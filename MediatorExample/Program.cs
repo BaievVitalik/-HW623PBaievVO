30fdf5eeea8a881742bba2ab4883ab4ba34b495e
Виталик Баев <v.o.baiev@student.khai.edu>
2025-06-11 08:02:40 +0300

    Last 2 test codes

---
+++ MediatorExample/Program.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace MediatorPatternDemo
+{
+
+    public interface IChatMediator
+    {
+        void SendMessage(string message, User sender);
+        void RegisterUser(User user);
+    }
+
+    public abstract class User
+    {
+        protected IChatMediator _mediator;
+        public string Name { get; }
+
+        public User(IChatMediator mediator, string name)
+        {
+            _mediator = mediator;
+            Name = name;
+        }
+
+        public abstract void Send(string message);
+        public abstract void Receive(string message);
+    }
+
+    public class ChatRoom : IChatMediator
+    {
+        private List<User> _users = new List<User>();
+
+        public void RegisterUser(User user)
+        {
+            _users.Add(user);
+        }
+
+        public void SendMessage(string message, User sender)
+        {
+            foreach (var user in _users)
+            {
+                if (user != sender)
+                {
+                    user.Receive(message);
+                }
+            }
+        }
+    }
+
+    public class ChatUser : User
+    {
+        public ChatUser(IChatMediator mediator, string name) : base(mediator, name)
+        {
+        }
+
+        public override void Send(string message)
+        {
+            Console.WriteLine($"[{Name}] відправляє: {message}");
+            _mediator.SendMessage(message, this);
+        }
+
+        public override void Receive(string message)
+        {
+            Console.WriteLine($"[{Name}] отримує: {message}");
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Демонстрація шаблону Mediator ---");
+
+            IChatMediator chatRoom = new ChatRoom();
+
+            User user1 = new ChatUser(chatRoom, "Олена");
+            User user2 = new ChatUser(chatRoom, "Іван");
+            User user3 = new ChatUser(chatRoom, "Петро");
+
+            chatRoom.RegisterUser(user1);
+            chatRoom.RegisterUser(user2);
+            chatRoom.RegisterUser(user3);
+
+            user1.Send("Привіт усім!");
+            Console.WriteLine();
+            user2.Send("Привіт, Олено! Як справи?");
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ ReactorExample/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+using System.Collections.Generic;
+
+
+namespace ReactorPatternDemo
+{
+
+    public class Reactor
+    {
+        private List<Socket> _socketsToMonitor = new List<Socket>();
+        private bool _isRunning = false;
+
+        public void AddSocket(Socket socket)
+        {
+            _socketsToMonitor.Add(socket);
+            Console.WriteLine($"[Reactor] Socket {socket.RemoteEndPoint} додано для моніторингу.");
+        }
+
+        public void Start()
+        {
+            _isRunning = true;
+            Console.WriteLine("[Reactor] Запущено. Очікування на події...");
+
+            while (_isRunning)
+            {
+                if (_socketsToMonitor.Count == 0)
+                {
+                    Thread.Sleep(100); 
+                    continue;
+                }
+
+                var checkRead = new List<Socket>(_socketsToMonitor);
+                var checkError = new List<Socket>(_socketsToMonitor);
+
+                try
+                {
+                    Socket.Select(checkRead, null, checkError, 1000000);
+
+                    foreach (var socket in checkRead)
+                    {
+                        HandleRead(socket);
+                    }
+
+                    foreach (var socket in checkError)
+                    {
+                        HandleError(socket);
+                    }
+                }
+                catch (SocketException ex)
+                {
+                    Console.WriteLine($"[Reactor] Помилка сокета: {ex.Message}");
+                }
+            }
+        }
+
+        public void Stop()
+        {
+            _isRunning = false;
+        }
+
+        private void HandleRead(Socket socket)
+        {
+            try
+            {
+                byte[] buffer = new byte[1024];
+                int received = socket.Receive(buffer, SocketFlags.None);
+                if (received > 0)
+                {
+                    string data = Encoding.UTF8.GetString(buffer, 0, received);
+                    Console.WriteLine($"[EventHandler] Отримано від {socket.RemoteEndPoint}: {data.Trim()}");
+
+                    byte[] response = Encoding.UTF8.GetBytes("Повідомлення отримано\n");
+                    socket.Send(response);
+                }
+                else 
+                {
+                    Console.WriteLine($"[EventHandler] Клієнт {socket.RemoteEndPoint} від'єднався.");
+                    socket.Close();
+                    _socketsToMonitor.Remove(socket);
+                }
+            }
+            catch (SocketException)
+            {
+                HandleError(socket);
+            }
+        }
+
+        private void HandleError(Socket socket)
+        {
+            Console.WriteLine($"[EventHandler] Помилка на сокеті {socket.RemoteEndPoint}. Закриття з'єднання.");
+            socket.Close();
+            _socketsToMonitor.Remove(socket);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("--- Демонстрація шаблону Reactor ---");
+
+            var listener = new Socket(AddressFamily.InterNetwork, SocketType.Stream, ProtocolType.Tcp);
+            var localEndPoint = new IPEndPoint(IPAddress.Any, 8888);
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+
+            Console.WriteLine("Сервер слухає на порту 8888...");
+            Console.WriteLine("Підключіться до сервера за допомогою telnet: 'telnet 127.0.0.1 8888'");
+
+            var reactor = new Reactor();
+            var reactorThread = new Thread(reactor.Start);
+            reactorThread.Start();
+
+            try
+            {
+                while (true)
+                {
+                    Socket clientSocket = listener.Accept();
+                    Console.WriteLine($"Прийнято нове з'єднання від {clientSocket.RemoteEndPoint}");
+
+                    reactor.AddSocket(clientSocket);
+                }
+            }
+            finally
+            {
+                reactor.Stop();
+                reactorThread.Join();
+                listener.Close();
+            }
+        }
+    }
+}
